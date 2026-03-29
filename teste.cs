87281365c3e9a8d74da7e758658dfb887801d314
Canducci <canducciboxing@gmail.com>
2026-03-29 16:13:28 -0300

    version2

--- teste.cs
+++ teste.cs
@@ -7,6 +7,7 @@ class Program
         Console.WriteLine("Projeto DevOps no GitHub!");
         Console.WriteLine("Nova funcionalidade adicionada!");
         Console.WriteLine("Versão 1");
+        Console.WriteLine("Versão 2 - alteração feita");
         
     }
 }
