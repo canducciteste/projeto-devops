ce7c5c7431e984df035ddc8a3f4caaab60185902
Canducci <canducciboxing@gmail.com>
2026-03-29 16:02:45 -0300

    Adicionando arquivo teste.cs com funcionalidade inicial

---
+++ teste.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Projeto DevOps no GitHub!");
+        Console.WriteLine("Nova funcionalidade adicionada!");
+        
+    }
+}
